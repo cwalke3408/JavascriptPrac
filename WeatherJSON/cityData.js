e0745052bb277c420f12db640361f7a1b1ab814e
syntel <syntel@PHX440G3-2815X0.syntelorg.com>
2018-05-03 15:02:06 -0400

    Create Contructors and Static examples

--- WeatherJSON/cityData.js
+++ WeatherJSON/cityData.js
@@ -1267,4 +1267,12 @@ const cities = {
     ]
  }
 
+//  module.exports = function (firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.fullName = function () { 
+//         return this.firstName + ' ' + this.lastName;
+//     }
+// }
+
  module.exports = cities;
