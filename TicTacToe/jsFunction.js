91dc7b1c1e6298e13c3eed5e8c258d43a2d92c9b
syntel <chrsitw@g.clemson.edu>
2018-05-07 12:01:11 -0400

    Create working Tic tac toe

--- TicTacToe/jsFunction.js
+++ TicTacToe/jsFunction.js
@@ -4,8 +4,22 @@
 //  - store them in variables
 
 let callback = function(){
-    console.log("Ev3ery Trhhee Seconds");
-    
+    console.log("Ev3ery Trhhee Seconds");   
 }
-
 setTimeout(callback, 3000);
+
+
+function a(x){
+    return function(y){
+        console.log(x+y);
+        return(x+y+1);
+    }
+}
+console.log(a(2)(3));
+
+// SETTIMEOUT
+// const setTimeout = function(callbackToRunLater, timeToWait){
+//     if (timeToWait has passed){
+//         callbackToRunLater()
+//     }
+// }
